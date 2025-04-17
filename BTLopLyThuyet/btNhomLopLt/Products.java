cc159d4ca8bef86f65b8e217b76119a81a242f72
Lương Văn Hưng <Hung.lv235740@sis.hust.edu.vn>
2025-04-17 10:10:21 +0700

    update bai tap code nhom 2

---
+++ BTLopLyThuyet/btNhomLopLt/Products.java
@@ -0,0 +1,48 @@
+public class Products {
+     int Products_id;
+     String name ;
+     int purchasePrice ;
+     int sellPrice ;
+     int quantity ;
+     double tax ;
+     public int getProducts_id() {
+         return Products_id;
+     }
+     public void setProducts_id(int products_id) {
+         Products_id = products_id;
+     }
+     public String getName() {
+         return name;
+     }
+     public void setName(String name) {
+         this.name = name;
+     }
+     public int getPurchasePrice() {
+         return purchasePrice;
+     }
+     public void setPurchasePrice(int purchasePrice) {
+         this.purchasePrice = purchasePrice;
+     }
+     public int getSellPrice() {
+         return sellPrice;
+     }
+     public void setSellPrice(int sellPrice) {
+         this.sellPrice = sellPrice;
+     }
+     public int getQuantity() {
+         return quantity;
+     }
+     public void setQuantity(int quantity) {
+         this.quantity = quantity;
+     }
+     public double getTax() {
+         return tax;
+     }
+     public void setTax(double tax) {
+         this.tax = tax;
+     }
+    
+}
+
+
+
